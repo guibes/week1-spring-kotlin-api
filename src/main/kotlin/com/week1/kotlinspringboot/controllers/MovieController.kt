95e2a45f3eb3b066f8a7a7eda62e483e4e17ea5c
Geovane Guibes <geovaneguibes@gmail.com>
2022-03-31 13:15:43 +0000

    feat: add new methods to controller [DAY-4-5]

--- src/main/kotlin/com/week1/kotlinspringboot/controllers/MovieController.kt
+++ src/main/kotlin/com/week1/kotlinspringboot/controllers/MovieController.kt
@@ -3,11 +3,16 @@ package com.week1.kotlinspringboot.movie
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/api/movie")
+@RequestMapping("/api/movies")
 class MovieController(var movieService: MovieService) {
 
     @GetMapping
     fun findMovies(): Iterable<Movie> {
         return movieService.findMovies()
+    }
+
+    @GetMapping("{id}")
+    fun findMovieById(@PathVariable id: Long): Movie {
+        return movieService.findMovieById(id)
     }
 }
