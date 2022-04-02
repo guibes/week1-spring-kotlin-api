3ec960374deb65dcaa2fe45d461a313ab3cb6bdc
Geovane Guibes <geovaneguibes@gmail.com>
2022-04-02 17:34:18 +0000

    test: add the tests for controller [DAY-6-7]

--- src/test/kotlin/com/week1/kotlinspringboot/controllers/MovieControllerTest.kt
+++ src/test/kotlin/com/week1/kotlinspringboot/controllers/MovieControllerTest.kt
@@ -1,13 +1,16 @@
 package com.week1.kotlinspringboot.movie
 
 import MovieFactory
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
 @SpringBootTest
@@ -15,6 +18,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 class MovieControllerTest {
         @Autowired lateinit var mockMvc: MockMvc
         @Autowired lateinit var movieService: MovieService
+        @Autowired lateinit var movieRepository: MovieRepository
 
         @Test
         fun `@GET movies route should return a empty list if does not exist movies`() {
@@ -29,41 +33,30 @@ class MovieControllerTest {
                                 .andExpect(MockMvcResultMatchers.content().string("[]"))
         }
 
-        // @Test
-        // fun `@GET movies route should return a movie if exists one in list`() {
-        //         var objectMapper: ObjectMapper =
-        //                         ObjectMapper().registerModule(JavaTimeModule())
-        //                                         .configure(
-        //                                                         SerializationFeature
-        //
-        // .WRITE_DATES_AS_TIMESTAMPS,
-        //                                                         false
-        //                                         )
-        //         var movie: Movie = MovieFactory().produce()
-        //         var createdMovie: List<Movie> = listOf(movieService.createMovie(movie))
+        @Test
+        fun `@GET movies route should return a movie if exists one in list`() {
+                var movie: Movie = MovieFactory().produce()
+                var createdMovie: List<Movie> = listOf(movieService.createMovie(movie))
 
-        //         mockMvc.perform(MockMvcRequestBuilders.get("/api/movies/"))
-        //                         .andExpect(MockMvcResultMatchers.status().isOk)
-        //                         .andExpect(
-        //                                         MockMvcResultMatchers.content()
-        //                                                         .contentType(
-        //
-        // MediaType.APPLICATION_JSON
-        //                                                         )
-        //                         )
-        //                         .andExpect(
-        //                                         MockMvcResultMatchers.content()
-        //                                                         .string(
-        //
-        // objectMapper.writeValueAsString(
-        //
-        // createdMovie
-        //                                                                         )
-        //                                                         )
-        //                         )
+                mockMvc.perform(MockMvcRequestBuilders.get("/api/movies/"))
+                                .andExpect(MockMvcResultMatchers.status().isOk)
+                                .andExpect(
+                                                MockMvcResultMatchers.content()
+                                                                .contentType(
+                                                                                MediaType.APPLICATION_JSON
+                                                                )
+                                )
+                                .andExpect(
+                                                MockMvcResultMatchers.content()
+                                                                .string(
+                                                                                ObjectMapper().writeValueAsString(
+                                                                                                                createdMovie
+                                                                                                )
+                                                                )
+                                )
 
-        //         movieService.deleteMovie(createdMovie[0].id ?: 1)
-        // }
+                movieRepository.delete(movie)
+        }
 
         @Test
         fun `@GET movies by id route should return a error if does not exists movies`() {
@@ -72,7 +65,7 @@ class MovieControllerTest {
         }
 
         @Test
-        fun `@GET movies route should return a movie if exists one in list`() {
+        fun `@GET movies route should return a movie if exists one by that id`() {
 
                 var movie: Movie = MovieFactory().produce()
                 var createdMovie: Movie = movieService.createMovie(movie)
@@ -88,6 +81,108 @@ class MovieControllerTest {
                                                                 )
                                 )
 
-                movieService.deleteMovie(movieId)
+                movieRepository.delete(movie)
         }
+
+        @Test
+        fun `@POST create movie need throw an bad request a movie if the body was missing`() {
+                mockMvc.perform(MockMvcRequestBuilders.post("/api/movies"))
+                                .andExpect(MockMvcResultMatchers.status().isBadRequest)
+        }
+
+        @Test
+        fun `@POST create movie need create a movie if all params are right`() {
+                var movie: Movie = MovieFactory().produce()
+                movie.id = 3
+                mockMvc.perform(
+                                                MockMvcRequestBuilders.post("/api/movies")
+                                                                .contentType(
+                                                                                MediaType.APPLICATION_JSON
+                                                                )
+                                                                .header(
+                                                                                HttpHeaders.ACCEPT,
+                                                                                MediaType.APPLICATION_JSON_VALUE
+                                                                )
+                                                                .content(
+                                                                                ObjectMapper().writeValueAsString(
+                                                                                                                movie
+                                                                                                )
+                                                                )
+                                )
+                                .andDo(MockMvcResultHandlers.print())
+                                .andExpect(MockMvcResultMatchers.status().isOk)
+                                .andExpect(
+                                                MockMvcResultMatchers.content()
+                                                                .contentType(
+                                                                                MediaType.APPLICATION_JSON
+                                                                )
+                                )
+                                .andExpect(
+                                                MockMvcResultMatchers.content()
+                                                                .string(
+                                                                                ObjectMapper().writeValueAsString(
+                                                                                                                movie
+                                                                                                )
+                                                                )
+                                )
+                movieRepository.delete(movie)
+        }
+
+        @Test
+        fun `@PATCH update movie need throw an bad request a movie if the body was missing`() {
+                mockMvc.perform(MockMvcRequestBuilders.patch("/api/movies/999"))
+                                .andExpect(MockMvcResultMatchers.status().isBadRequest)
+        }
+
+        @Test
+        fun `@PATCH update movie need throw a not found if movie does not exists and have a body`() {
+                mockMvc.perform(
+                                                MockMvcRequestBuilders.patch("/api/movies/999")
+                                                                .contentType(
+                                                                                MediaType.APPLICATION_JSON
+                                                                )
+                                                                .content("{\"title\": \"test\"}")
+                                )
+                                .andExpect(MockMvcResultMatchers.status().isNotFound)
+        }
+
+        @Test
+        fun `@PATCH update movie need update a movie if all the params are right`() {
+                val movie: Movie = MovieFactory().produce()
+                val createdMovie: Movie = movieRepository.save(movie)
+                mockMvc.perform(
+                                                MockMvcRequestBuilders.patch(
+                                                                                "/api/movies/" +
+                                                                                                createdMovie.id
+                                                                )
+                                                                .contentType(
+                                                                                MediaType.APPLICATION_JSON
+                                                                )
+                                                                .content("{\"title\": \"test\"}")
+                                )
+                                .andExpect(MockMvcResultMatchers.status().isOk)
+                                .andExpect(
+                                                MockMvcResultMatchers.content()
+                                                                .json(
+                                                                              "{\"title\":\"test\",\"overview\":\"" +
+                                                                                                movie.overview +
+                                                                                                "\",\"genre\":\"" +
+                                                                                                movie.genre +
+                                                                                                "\",\"posterPath\":\"" +
+                                                                                                movie.posterPath +
+                                                                                                "\",\"id\":" +
+                                                                                                movie.id +
+                                                                                                "}"
+                                                                )
+                                )
+        }
+
+        @Test
+        fun `@DELETE delete movie need throw an not found if the movie does not exists was missing`() {
+                val movie: Movie = MovieFactory().produce()
+                val createdMovie: Movie = movieRepository.save(movie)
+                mockMvc.perform(MockMvcRequestBuilders.delete("/api/movies/" + createdMovie.id))
+                                .andExpect(MockMvcResultMatchers.status().isOk)
+        }
+
 }
