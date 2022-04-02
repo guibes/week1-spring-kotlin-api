531e93c2831b6b215d077c7ad9ae77a3ef00a61c
Geovane Guibes <geovaneguibes@gmail.com>
2022-04-02 14:52:33 -0500

    test: fix a test broken [DAY-6-7]

--- src/test/kotlin/com/week1/kotlinspringboot/services/MovieServiceTest.kt
+++ src/test/kotlin/com/week1/kotlinspringboot/services/MovieServiceTest.kt
@@ -85,7 +85,7 @@ class MovieServiceTest {
     fun `deleteMovie(id) should call its method and need delete the movie`() {
         var id: Long = 123
         // given
-        every { movieRepository.deleteById(id) } returns Unit
+        every { movieRepository.deleteById(id) } returns ""
         // where
         movieService.deleteMovie(id)
         // then
